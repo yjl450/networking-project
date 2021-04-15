2cb2f5a03823364d0d4816acb69c3c87311931f6
Yijian Liu <32358997+yjl450@users.noreply.github.com>
2021-04-15 17:40:36 +0800

    Create Main.vue

---
+++ frontend/src/views/Main.vue
@@ -0,0 +1,44 @@
+<template>
+  <div>main page</div>
+</template>
+
+<script>
+// color palette https://flatuicolors.com/palette/us
+
+// @ is an alias to /src
+// import HelloWorld from '@/components/HelloWorld.vue'
+import Vue from "vue";
+import VueMaterial from "vue-material";
+import "vue-material/dist/vue-material.min.css";
+import "vue-material/dist/theme/default.css";
+
+Vue.use(VueMaterial);
+
+export default {
+  name: "Main",
+  components: {},
+  data() {
+    return {
+      id: -1,
+      username: "",
+    };
+  },
+  computed: {},
+  created() {
+    //   Back to login page if no login token
+    // if (window.sessionStorage.getItem("id")) {
+    //   this.id = window.sessionStorage.getItem("id");
+    //   this.username = window.sessionStorage.getItem("username");
+    // } else {
+    //   this.$router.push("/");
+    // }
+  },
+  mounted() {},
+  methods: {
+    submit() {},
+  },
+};
+</script>
+
+<style scoped>
+</style>
